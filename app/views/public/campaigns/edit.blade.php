d4bb1ddd2f96072b9a67cdfc3a5b999f237ac3ea
Simon Depelchin <simon@reaklab.com>
2014-08-02 10:51:54 +0200

    Refactoring de campaigns

--- app/views/public/campaigns/edit.blade.php
+++ app/views/public/campaigns/edit.blade.php
@@ -1,4 +1,4 @@
-<h2 class="page-header">@lang('campaign.edit.title')</h2>
+<h2 class="page-header">@lang('campaigns.edit.title')</h2>
 
 @if( Session::has( 'message' ) )
 <div class="alert alert-{{ $errors->has() ? 'danger' : 'success' }} alert-dismissible" role="alert">
@@ -7,12 +7,12 @@
 </div>
 @endif
 
-<form action="{{ URL::route('public.campaign.create') }}" method="post">
+<form action="{{ URL::route('public.campaigns.update', [ 'id' => $campaign->id ]) }}" method="post">
     <div class="row">
         <div class="col-md-6">
             <div class="form-group {{ $errors->has('title') ? 'has-error has-feedback' : '' }}">
-                <label for="name">@lang('campaign.form.labels.title')</label>
-                <input type="text" class="form-control" name="title" id="title" value="{{ Input::old('title', $campaign->title) }}" placeholder="@lang('campaign.form.placeholders.title')">
+                <label for="name">@lang('campaigns.form.labels.title')</label>
+                <input type="text" class="form-control" name="title" id="title" value="{{ Input::old('title', $campaign->title) }}" placeholder="@lang('campaigns.form.placeholders.title')">
                 @if( $errors->has('title' ) )
                 <span class="glyphicon glyphicon-remove form-control-feedback"></span>
                 <span class="help-block">{{ $errors->first('title') }}</span>
@@ -21,7 +21,7 @@
         </div>
         <div class="col-md-6">
             <div class="form-group {{ $errors->has('description') ? 'has-error has-feedback' : '' }}">
-                <label for="name">@lang('campaign.form.labels.description')</label>
+                <label for="name">@lang('campaigns.form.labels.description')</label>
                 <textarea class="form-control" rows="3" name="description" id="description" value="{{ Input::old('description', $campaign->description) }}"></textarea>
                 @if( $errors->has('description' ) )
                 <span class="glyphicon glyphicon-remove form-control-feedback"></span>
@@ -31,8 +31,8 @@
         </div>
         <div class="col-md-6">
             <div class="form-group {{ $errors->has('date_start') ? 'has-error has-feedback' : '' }}">
-                <label for="name">@lang('campaign.form.labels.date_start')</label>
-                <input type="date" class="form-control" name="date_start" id="date_start" value="{{ Input::old('date_start', $campaign->date_start) }}" placeholder="@lang('campaign.form.placeholders.date_start')">
+                <label for="name">@lang('campaigns.form.labels.date_start')</label>
+                <input type="date" class="form-control" name="date_start" id="date_start" value="{{ Input::old('date_start', $campaign->date_start) }}" placeholder="@lang('campaigns.form.placeholders.date_start')">
                 @if( $errors->has('date_start' ) )
                 <span class="glyphicon glyphicon-remove form-control-feedback"></span>
                 <span class="help-block">{{ $errors->first('date_start') }}</span>
@@ -41,8 +41,8 @@
         </div>
         <div class="col-md-6">
             <div class="form-group {{ $errors->has('date_end') ? 'has-error has-feedback' : '' }}">
-                <label for="name">@lang('campaign.form.labels.date_end')</label>
-                <input type="date" class="form-control" name="date_end" id="date_end" value="{{ Input::old('date_end', $campaign->date_end) }}" placeholder="@lang('campaign.form.placeholders.date_end')">
+                <label for="name">@lang('campaigns.form.labels.date_end')</label>
+                <input type="date" class="form-control" name="date_end" id="date_end" value="{{ Input::old('date_end', $campaign->date_end) }}" placeholder="@lang('campaigns.form.placeholders.date_end')">
                 @if( $errors->has('date_end' ) )
                 <span class="glyphicon glyphicon-remove form-control-feedback"></span>
                 <span class="help-block">{{ $errors->first('date_end') }}</span>
@@ -51,7 +51,7 @@
         </div>
         <div class="col-md-6">
             <div class="form-group {{ $errors->has('item_title') ? 'has-error has-feedback' : '' }}">
-                <label for="name">@lang('campaign.form.labels.item_title')</label>
+                <label for="name">@lang('campaigns.form.labels.item_title')</label>
                 <input type="text" class="form-control" name="item_title" id="item_title" value="{{ Input::old('item_title', $campaign->item_title) }}" placeholder="@lang('campaign.form.placeholders.item_title')">
                 @if( $errors->has('item_title' ) )
                 <span class="glyphicon glyphicon-remove form-control-feedback"></span>
@@ -72,7 +72,7 @@
         <div class="col-md-6">
             <div class="form-group {{ $errors->has('item_price') ? 'has-error has-feedback' : '' }}">
                 <label for="name">@lang('campaign.form.labels.item_price')</label>
-                <input type="text" class="form-control" name="item_price" id="item_price" value="{{ Input::old('item_price', $campaign->item_price) }}" placeholder="@lang('campaign.form.placeholders.item_price')">
+                <input type="text" class="form-control" name="item_price" id="item_price" value="{{ Input::old('item_price', round($campaign->item_price / 100, 2)) }}" placeholder="@lang('campaign.form.placeholders.item_price')">
                 @if( $errors->has('item_price' ) )
                 <span class="glyphicon glyphicon-remove form-control-feedback"></span>
                 <span class="help-block">{{ $errors->first('item_price') }}</span>
@@ -131,8 +131,8 @@
         </div>
         <div class="col-md-6">
             <div class="form-group {{ $errors->has('target_adress_country') ? 'has-error has-feedback' : '' }}">
-                <label for="name">@lang('campaign.form.labels.target_adress_country')</label>
-                <input type="text" class="form-control" name="target_adress_country" id="target_adress_country" value="{{ Input::old('target_adress_country', $campaign->target_adress_country) }}" placeholder="@lang('campaign.form.placeholders.target_adress_country')">
+                <label for="name">@lang('campaigns.form.labels.target_adress_country')</label>
+                <input type="text" class="form-control" name="target_adress_country" id="target_adress_country" value="{{ Input::old('target_adress_country', $campaign->target_adress_country) }}" placeholder="@lang('campaigns.form.placeholders.target_adress_country')">
                 @if( $errors->has('target_adress_country' ) )
                 <span class="glyphicon glyphicon-remove form-control-feedback"></span>
                 <span class="help-block">{{ $errors->first('target_adress_country') }}</span>
@@ -141,7 +141,7 @@
         </div>
         <div class="col-md-6">
             <div class="form-group {{ $errors->has('item_target_description') ? 'has-error has-feedback' : '' }}">
-                <label for="name">@lang('campaign.form.labels.target_description')</label>
+                <label for="name">@lang('campaigns.form.labels.target_description')</label>
                 <textarea class="form-control" rows="3" name="target_description" id="target_description" value="{{ Input::old('target_description', $campaign->target_description) }}"></textarea>
                 @if( $errors->has('target_description' ) )
                 <span class="glyphicon glyphicon-remove form-control-feedback"></span>
@@ -151,5 +151,5 @@
         </div>
     </div>
     {{ Form::token() }}
-    <button class="btn btn-primary"><i class="glyphicon glyphicon-ok"></i> @lang('campaign.form.edit.submit')</button>
+    <button class="btn btn-primary"><i class="glyphicon glyphicon-ok"></i> @lang('campaigns.form.edit.submit')</button>
 </form>
