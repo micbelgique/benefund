07720ad745b21fb50681d48dc8fcd03416504137
Kévin Rapaille <kevin.rapaille@hotmail.com>
2014-08-02 10:55:13 +0200

    Merge branch 'master' of https://github.com/micdevcamp/benefund
    Conflicts:
    	app/lang/en/campaigns.php

---
+++ app/views/public/campaigns/index.blade.php
@@ -0,0 +1,73 @@
+<h2 class="campaign-header">@lang('campaigns.title')</h2>
+
+@if( Session::has('message') )
+<div class="alert alert-info">{{ Session::get('message') }}</div>
+@endif
+
+<div class="row">
+    <div class="col-xs-12">
+        @if( Input::get('s', '') != '' )
+        <div class="pull-left">
+            <h4>@lang('campaigns.search.title', [ 'search' => Input::get('s') ])</h4>
+        </div>
+        @endif
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-12">
+        <div class="pull-left">
+            <p><a href="{{ URL::route('public.campaigns.new') }}" class="btn btn-primary"><i class="glyphicon glyphicon-plus"></i> @lang('campaigns.form.buttons.new')</a></p>
+        </div>
+        <div class="pull-right">
+            <p>
+                <form action="{{ URL::route('public.campaigns') }}" type="get" class="form-inline">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="s" id="s" placeholder="@lang('campaigns.form.search.placeholder')" value="{{ Input::get('s', '') }}">
+                        <span class="input-group-btn">
+                            <button class="btn btn-primary"><i class="glyphicon glyphicon-search"></i> @lang('campaigns.form.search.button')</button>
+                        </span>
+                    </div>
+                </form>
+            </p>
+        </div>
+    </div>
+</div>
+
+<table id="campaigns" class="table table-striped table-hover table-bordered">
+    <thead>
+        <tr>
+            <th>@lang('campaigns.title')</th>
+            <th>@lang('app.actions')</th>
+        </tr>
+    </thead>
+    <tbody>
+    @if( count( $campaigns ) > 0 )
+        @foreach( $campaigns as $index => $campaign )
+        <tr id="campaign-{{ $campaign->id }}">
+            <td>{{ $campaign->title }}</td>
+            <td>
+                <form action="{{ URL::route('public.campaigns.delete', [$campaign->id]) }}" method="post">
+                    <div class="btn-group btn-group-block">
+                        <a href="{{ URL::route('public.campaigns.edit', [$campaign->id]) }}" class="btn btn-primary btn-sm btn-col-4"><i class="glyphicon glyphicon-edit"></i> <span class="hidden-xs">@lang('campaigns.form.buttons.edit')</span></a>
+                        <button class="btn btn-danger btn-sm btn-col-4"><i class="glyphicon glyphicon-trash"></i> <span class="hidden-xs">@lang('campaigns.form.buttons.remove')</span></button>
+                    </div>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    @else
+        <tr class="danger">
+            <td colspan="3">@lang('campaigns.empty')</td>
+        </tr>
+    @endif
+    </tbody>
+</table>
+
+<div class="row">
+    <div class="col-xs-12">
+        <div class="pull-left">
+            <p><a href="{{ URL::route('public.campaigns.new') }}" class="btn btn-primary"><i class="glyphicon glyphicon-plus"></i> @lang('campaigns.form.buttons.new')</a></p>
+        </div>
+    </div>
+</div>
--- app/views/public/parts/menu.blade.php
+++ app/views/public/parts/menu.blade.php
@@ -11,12 +11,11 @@
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="{{ URL::route('home') }}">Home</a></li>
+                <li class="active"><a href="{{ URL::route('home') }}">@lang('menu.home')</a></li>
+                <li><a href="{{ URL::route('public.campaigns') }}">@lang('menu.campaigns')</a></li>
                 @if( ! Auth::check() )
-                <li><a href="{{ URL::route('login') }}">Login</a></li>
-                <li><a href="{{ URL::route('register') }}">Register</a></li>
-                @else
-                <li><a href="{{ URL::route('logout') }}">Logout</a></li>
+                <li><a href="{{ URL::route('login') }}">@lang('menu.login')</a></li>
+                <li><a href="{{ URL::route('register') }}">@lang('menu.register')</a></li>
                 @endif
             </ul>
             @if( Auth::check() )
@@ -24,7 +23,7 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <span class="glyphicon glyphicon-user"></span> 
-                        <strong>Profile</strong>
+                        <strong>@lang('menu.profile')</strong>
                         <span class="glyphicon glyphicon-chevron-down"></span>
                     </a>
                     <ul class="dropdown-menu">
@@ -33,14 +32,14 @@
                                 <div class="row">
                                     <div class="col-lg-4">
                                         <p class="text-center">
-                                            {{ Auth::user()->get_avatar_url() ? '<img src="' . Auth::user()->get_avatar_url() . '" alt="Avatar" class="img-circle">' : '<span class="glyphicon glyphicon-user icon-size"></span>' }}
+                                            {{ Auth::user()->get_avatar_url() ? '<img src="' . Auth::user()->get_avatar_url() . '" alt="' . Lang::get('user.avatar') . '" class="img-circle">' : '<span class="glyphicon glyphicon-user icon-size"></span>' }}
                                         </p>
                                     </div>
                                     <div class="col-lg-8">
                                         <p class="text-left"><strong>{{ Auth::user()->first_name . ' ' . Auth::user()->last_name }}</strong></p>
                                         <p class="text-left small">{{ Auth::user()->email }}</p>
                                         <p class="text-left">
-                                            <a href="{{ URL::route('profile') }}" class="btn btn-primary btn-block btn-sm">Edit profile</a>
+                                            <a href="{{ URL::route('profile') }}" class="btn btn-primary btn-block btn-sm">@lang('menu.edit_profile')</a>
                                         </p>
                                     </div>
                                 </div>
@@ -52,11 +51,11 @@
                                 <div class="row">
                                     <div class="col-lg-12">
                                         <p>
-                                            <a href="{{ URL::route('logout') }}" class="btn btn-danger btn-block">Logout</a>
+                                            <a href="{{ URL::route('logout') }}" class="btn btn-danger btn-block">@lang('menu.logout')</a>
                                         </p>
                                         @if( Auth::user()->role()->first()->name_tag == 'admin' )
                                         <p>
-                                            <a href="{{ URL::route('admin.home') }}" class="btn btn-info btn-block">Administration</a>
+                                            <a href="{{ URL::route('admin.home') }}" class="btn btn-info btn-block">@lang('menu.administration')</a>
                                         </p>
                                         @endif
                                     </div>
