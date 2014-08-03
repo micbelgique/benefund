620722d6d463d68d011eebee643f67aa056a4274
Simon Depelchin <simon@reaklab.com>
2014-08-03 12:32:44 +0200

    Gestion de la création de pledges

--- app/views/public/campaigns/sidebars/edit.blade.php
+++ app/views/public/campaigns/sidebars/edit.blade.php
@@ -1,6 +1,6 @@
 <div class="panel panel-default">
     <div class="panel-heading">
-        <h3>@lang('campaigns.sidebar.pledges.title')</h3>
+        <h4>@lang('campaigns.sidebar.pledges.title')</h4>
     </div>
     <div class="panel-body">
         <p>@lang('campaigns.sidebar.pledges.description')</p>
@@ -33,11 +33,12 @@
             </tr>
             <tr>
                 <td colspan="2">
-                    <input type="text" name="title" id="pledge_title" class="form-control {{ 'create_pledge' == Input::get('action', '') && $errors->has('title') ? 'has-error has-feedback' : '' }}" value="{{ 'create_pledge' == Input::get('action', '') ? Input::old('title', '') : '' }}">
-                    @if( 'create_pledge' == Input::get('action', '') && $errors->has('title' ) )
-                    <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-                    <span class="help-block">{{ $errors->first('title') }}</span>
-                    @endif
+                    <div class="form-group {{ 'create_pledge' == Input::old('action', '') && $errors->has('title') ? 'has-error' : '' }}">
+                        <input type="text" name="title" id="pledge_title" class="form-control" value="{{ 'create_pledge' == Input::old('action', '') ? Input::old('title', '') : '' }}">
+                        @if( 'create_pledge' == Input::old('action', '') && $errors->has('title' ) )
+                        <span class="help-block">{{ $errors->first('title') }}</span>
+                        @endif
+                    </div>
                 </td>
             </tr>
             <tr>
@@ -45,11 +46,12 @@
             </tr>
             <tr>
                 <td colspan="2">
-                    <textarea name="description" id="pledge_description" class="form-control {{ 'create_pledge' == Input::get('action', '') && $errors->has('description') ? 'has-error has-feedback' : '' }}">{{ 'create_pledge' == Input::get('action', '') ? Input::old('description', '') : '' }}</textarea>
-                    @if( 'create_pledge' == Input::get('action', '') && $errors->has('description' ) )
-                    <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-                    <span class="help-block">{{ $errors->first('description') }}</span>
-                    @endif
+                    <div class="form-group {{ 'create_pledge' == Input::old('action', '') && $errors->has('description') ? 'has-error' : '' }}">
+                        <textarea name="description" id="pledge_description" class="form-control {{ 'create_pledge' == Input::old('action', '') && $errors->has('description') ? 'has-error' : '' }}">{{ 'create_pledge' == Input::old('action', '') ? Input::old('description', '') : '' }}</textarea>
+                        @if( 'create_pledge' == Input::old('action', '') && $errors->has('description' ) )
+                        <span class="help-block">{{ $errors->first('description') }}</span>
+                        @endif
+                    </div>
                 </td>
             </tr>
             <tr>
@@ -58,18 +60,20 @@
             </tr>
             <tr>
                 <td>
-                    <input type="text" name="price_min" id="pledge_price_min" class="form-control {{ 'create_pledge' == Input::get('action', '') && $errors->has('price_min') ? 'has-error has-feedback' : '' }}" value="{{ 'create_pledge' == Input::get('action', '') ? Input::old('price_min', '') : '' }}">
-                    @if( 'create_pledge' == Input::get('action', '') && $errors->has('price_min' ) )
-                    <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-                    <span class="help-block">{{ $errors->first('price_min') }}</span>
-                    @endif
+                    <div class="form-group {{ 'create_pledge' == Input::old('action', '') && $errors->has('price_min') ? 'has-error' : '' }}">
+                        <input type="number" min="0" name="price_min" id="pledge_price_min" class="form-control {{ 'create_pledge' == Input::old('action', '') && $errors->has('price_min') ? 'has-error' : '' }}" value="{{ 'create_pledge' == Input::old('action', '') ? Input::old('price_min', '0') : '0' }}">
+                        @if( 'create_pledge' == Input::old('action', '') && $errors->has('price_min' ) )
+                        <span class="help-block">{{ $errors->first('price_min') }}</span>
+                        @endif
+                    </div>
                 </td>
                 <td>
-                    <input type="text" name="price_max" id="pledge_price_max" class="form-control {{ 'create_pledge' == Input::get('action', '') && $errors->has('price_max') ? 'has-error has-feedback' : '' }}" value="{{ 'create_pledge' == Input::get('action', '') ? Input::old('price_max', '') : '' }}">
-                    @if( 'create_pledge' == Input::get('action', '') && $errors->has('price_max' ) )
-                    <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-                    <span class="help-block">{{ $errors->first('price_max') }}</span>
-                    @endif
+                    <div class="form-group {{ 'create_pledge' == Input::old('action', '') && $errors->has('price_max') ? 'has-error' : '' }}">
+                        <input type="number" min="0" name="price_max" id="pledge_price_max" class="form-control {{ 'create_pledge' == Input::old('action', '') && $errors->has('price_max') ? 'has-error' : '' }}" value="{{ 'create_pledge' == Input::old('action', '') ? Input::old('price_max', '') : '' }}">
+                        @if( 'create_pledge' == Input::old('action', '') && $errors->has('price_max' ) )
+                        <span class="help-block">{{ $errors->first('price_max') }}</span>
+                        @endif
+                    </div>
                 </td>
             </tr>
             <tr>
